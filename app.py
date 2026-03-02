a642681a5ca05eca5846f6cfa1ce53da7a254219
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-02 23:25:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ from models import db, User, Snippet
 from forms import RegisterForm, LoginForm, SnippetForm
 
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "supersecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///snippets.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+with app.app_context():
+    db.create_all()
+
