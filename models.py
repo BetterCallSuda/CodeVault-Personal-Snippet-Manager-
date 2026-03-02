590c92635f43ea767681b07a6384d8c7a3418e13
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-02 23:23:25 +0530

    Update models.py

--- models.py
+++ models.py
@@ -11,3 +11,12 @@ class User(UserMixin, db.Model):
 
     snippets = db.relationship("Snippet", back_populates="owner", cascade="all, delete")
 
+class Snippet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    code = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    owner = db.relationship("User", back_populates="snippets")
