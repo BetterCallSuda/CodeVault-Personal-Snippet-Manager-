80d206c35041a41f94e54cf0607a9daafc8fc1f8
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-02 23:23:18 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,3 +4,10 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    snippets = db.relationship("Snippet", back_populates="owner", cascade="all, delete")
+
